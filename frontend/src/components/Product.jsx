a259e45c55388d02882cf8cdab157c05e1c5dbb4
benhazoom <87472603+benhazoom@users.noreply.github.com>
2024-07-29 10:58:25 +0300

    Added rating component and routing packages installed

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,20 +1,26 @@
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Rating from './Rating';
 
-//taking product as a prop as beeing mapped from HomeScreen
+
 const Product = ({ product }) => {
   return (
-    //try to play with classes
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
-      </a>
+      </Link>
 
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
+          {/* //if you want equal card sizes for products you can use this instead - <Card.Title as='div' className='product-title'> */}
           <Card.Title as='div'>
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
+        
+        <Card.Text as='div'>
+          <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+        </Card.Text>
 
         <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
@@ -23,6 +29,3 @@ const Product = ({ product }) => {
 };
 
 export default Product;
-
-//Notes
-//1.Card.X - X are different properties of Card see more in bootstrap.Card
