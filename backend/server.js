29bcff793c083567766ec9393476158cbe9df2b6
benhazoom <87472603+benhazoom@users.noreply.github.com>
2024-07-31 15:14:34 +0300

    changing misspelled backend folder

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv'
-dotenv.config();
-const port = process.env.PORT || 5000;
-import mongoose from "mongoose"
 import productRoutes from "./routes/productRoutes.js"
 import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from './config/db.js';
 
+dotenv.config();
+const port = process.env.PORT || 5000;
 const app = express();
 
-
-const connectDB = mongoose
-  .connect(process.env.MONGO_URI)
-  .then((result) => {
-    console.log(`MongoDB Connected: ${result.connection.host}`);
-  })
-  .catch((err) => console.log(err));
+connectDB();
 
 app.use ('/api/products',productRoutes);//when going to this route api/products will be processed via productRoutes
 
@@ -24,4 +18,6 @@ app.get('/', (req, res) => {
 
 app.use(notFound);
 app.use(errorHandler);
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
