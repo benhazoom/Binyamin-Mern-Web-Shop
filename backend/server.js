534acb0955c2cc06d809bf53027dd3b5015f2127
benhazoom <87472603+benhazoom@users.noreply.github.com>
2024-08-07 16:23:42 +0300

    user routed added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import productRoutes from "./routes/productRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from './config/db.js';
 
@@ -11,6 +12,7 @@ const app = express();
 connectDB();
 
 app.use ('/api/products',productRoutes);//when going to this route api/products will be processed via productRoutes
+app.use ('/api/users',userRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
