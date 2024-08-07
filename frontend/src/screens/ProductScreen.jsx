af5830396a8219483ec70f661f992903559899d9
benhazoom <87472603+benhazoom@users.noreply.github.com>
2024-08-07 15:39:34 +0300

    cart functionallity added

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,16 +1,35 @@
-import { useParams } from "react-router-dom";
+import { useParams,useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import {Row,Col,Image,ListGroup,Card,Button} from "react-bootstrap";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
 import { useGetProductDetailsQuery } from "../slices/productApiSlice";
-import Loader from '../components/Loader';
+import Loader from "../components/Loader";
 import Message from "../components/Message";
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+import { addToCart } from "../slices/cartSlice";
+import { useDispatch } from "react-redux";
 
 const ProductScreen = () => {
-
   const { id: productId } = useParams();
-  const {data:product,isLoading,error} = useGetProductDetailsQuery(productId);
-  
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  // Declare a state variable `qty` and a function `setQty` to update it.
+  // Initialize `qty` with the value 1.
+  const [qty, setQty] = useState(1);
+
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useGetProductDetailsQuery(productId);
+
+  //a method to dispatching the addToCart action
+  const addToCartHandler = () => {
+    dispatch(addToCart({...product,qty}));//we deconstruct so we could use qty as part of the item properties in the cartSlice reducer
+    navigate('/cart')
+  }
 
   return (
     <>
@@ -18,78 +37,109 @@ const ProductScreen = () => {
         Go Back
       </Link>
 
-      { isLoading? (
-      <Loader/>
-    ):error? (<Message variant='danger'>
-      {error?.data?.message || error.error}
-    </Message>): (
-      <>
-      <Row>
-        <Col md={5}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <Row>
+            <Col md={5}>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <h3>{product.name}</h3>
+                </ListGroup.Item>
 
-            <ListGroup.Item>
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} reviews`}
-              />
-            </ListGroup.Item>
+                <ListGroup.Item>
+                  <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                  />
+                </ListGroup.Item>
 
-            {/* <ListGroup.Item>Price: ${product.price}</ListGroup.Item> */}
-          </ListGroup>
-        </Col>
-        <Col md={1}></Col>
-        <Col md={4}>
-          <Card>
-            <ListGroup variant="flush">
-              <ListGroup.Item>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-            </ListGroup>
-            <ListGroup variant="flush">
-              <ListGroup.Item>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    <strong>
-                      {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
-                    </strong>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
+                {/* <ListGroup.Item>Price: ${product.price}</ListGroup.Item> */}
+              </ListGroup>
+            </Col>
+            <Col md={1}></Col>
+            <Col md={4}>
+              <Card>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Price:</Col>
+                      <Col>
+                        <strong>${product.price}</strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                </ListGroup>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status:</Col>
+                      <Col>
+                        <strong>
+                          {product.countInStock > 0
+                            ? "In Stock"
+                            : "Out of Stock"}
+                        </strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
 
-              <ListGroup.Item>
-                <Button
-                  className="btn-block"
-                  type="button"
-                  disabled={product.countInStock === 0}
-                >
-                  Add To Cart
-                </Button>
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </Col>
-        <Col md={2}></Col>
-      </Row>
-      <br />
-      <br />
-      <Row>
-        <Col md={5}>
-          <Image src={product.image} alt={product.name}></Image>
-        </Col>
-      </Row>
-    </>
-    )}
+                  {product.countInStock > 0 && (
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Qty</Col>
+                        <Col>
+                          <Form.Control
+                            as="select"
+                            value={qty}
+                            onChange={(e) => setQty(Number(e.target.value))}
+                          >
+                            {/* makes an Array of the numbers between 1 to product.countInStock*/}
+                            {[
+                              ...Array(product.countInStock)
+                                .keys()
+                                .map((x) => (
+                                  <option key={x + 1} value={x + 1}>
+                                    {x + 1}
+                                  </option>
+                                )),
+                            ]}
+                          </Form.Control>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  )}
+
+                  <ListGroup.Item>
+                    <Button
+                      className="btn-block"
+                      type="button"
+                      disabled={product.countInStock === 0}
+                      onClick={addToCartHandler}
+                    >
+                      Add To Cart
+                    </Button>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Col>
+            <Col md={2}></Col>
+          </Row>
+          <br />
+          <br />
+          <Row>
+            <Col md={5}>
+              <Image src={product.image} alt={product.name}></Image>
+            </Col>
+          </Row>
         </>
+      )}
+    </>
   );
 };
 
---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
+
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : { cartItems: [] };
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    //state is the current state of the cart,
+    //action is the payload - item to add to the cart
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existItem = state.cartItems.find(
+        (cartItem) => cartItem._id === item._id
+      );
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((cartItem) =>
+          cartItem._id === existItem._id ? item : cartItem
+        );
+      } else {
+        //were not typing like this because state is imutable
+        // state.cartItems = state.cartItems.push(item);
+        //instead we make a copy and assigning it to the state
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      return updateCart(state);
+    },
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter((cartItem) => cartItem._id !== action.payload
+      );
+
+      return updateCart(state);
+    },
+  },
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
