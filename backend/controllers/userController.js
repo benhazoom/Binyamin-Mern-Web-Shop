1db2f17435e1f8e210d41536f24088876252f5c4
benhazoom <87472603+benhazoom@users.noreply.github.com>
2024-08-08 14:39:31 +0300

    Logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,7 +48,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Privet
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  //logout means essentially clearing cookies
+  res.cookie('jwt','',{
+    httpOnly:true,
+    expires: new Date(0)
+  });
+  res.status(200).json({ message: 'Logged out successfully'})
 });
 
 // @desc    Get user profile
