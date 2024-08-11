3dacc2d7129a86677540687e3fc8ff1885e17c19
benhazoom <87472603+benhazoom@users.noreply.github.com>
2024-08-11 14:02:44 +0300

    added Logout functionality

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,7 +1,7 @@
 import { USERS_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
-// Extends the existing apiSlice by injecting additional endpoints
+// Extends the existing apiSlice by injecting additional endpoints to comunicate with the server
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         // Adds a login mutation because we are making a post request 
@@ -12,11 +12,15 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 body:data,
 
             }),
-            // Keeps unused data in the cache for 5 seconds
-            keepUnusedDataFor: 5,
+        }),
+        logout: builder.mutation({
+            query: () => ({
+                url: `${USERS_URL}/logout`,
+                method:'POST',
+            }),
         }),
     })
 });
 
 // Exports a custom hook for the getProducts query
-export const { useLoginMutation} = usersApiSlice;
+export const { useLoginMutation,useLogoutMutation} = usersApiSlice;
