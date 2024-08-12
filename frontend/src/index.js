5d5ff60978ecedd1ed51d9e00d94ac96c95e1bb5
benhazoom <87472603+benhazoom@users.noreply.github.com>
2024-08-12 10:38:27 +0300

    added privet routes
    to be used only by signed in users

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,43 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import './assets/styles/bootstrap.custom.css';//greenish greyish custom bootstrap css file
-import './assets/styles/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./assets/styles/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 //ReactRouter imports
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from 'react-router-dom';
+} from "react-router-dom";
 //redux
 import { Provider } from "react-redux";
-import store from './store';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/CartScreen';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import ShippingScreen from './screens/ShippingScreen';
+import store from "./store";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PrivetRoute from "./components/PrivetRoute";
 
 //defines what rout3 will render what screen
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route index={true} path='/' element={<HomeScreen />} />
-      <Route path='/product/:id'   element={<ProductScreen />} />
-      <Route path='/cart'   element={<CartScreen />} />
-      <Route path='/login'   element={<LoginScreen />} />
-      <Route path='/register'   element={<RegisterScreen />} />
-      <Route path='/shipping'   element={<ShippingScreen />} />
+    <Route path="/" element={<App />}>
+      {/* public routes */}
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
 
+      {/* private routes */}
+      <Route path="" element={<PrivetRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
